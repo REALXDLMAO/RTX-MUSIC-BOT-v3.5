96324053663a5ae6afab8cc81da05b04617c2a67
REALXDLMAO <157639208+REALXDLMAO@users.noreply.github.com>
2024-01-26 00:59:25 +0400

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,1 +1,60 @@
-/*LOLXDMAJDJJWEVFJEVFKOWEVFOGWREOPWERGPPREFPPEWRPGRPWREPPGWERPERGWPGPREPEWGPGERWPPEWGTEPTHPHWTE
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+   
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
+const { ApplicationCommandOptionType } = require('discord.js');
+const db = require("../mongoDB");
+
+module.exports = {
+  name: "bot",
+  description: "Get information about bot.",
+  permissions: "0x0000000000000800",
+  options: [],
+
+  run: async (client, interaction) => {
+    try {
+      const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
+      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const { EmbedBuilder } = require('discord.js')
+        const embed = new EmbedBuilder()
+            .setColor('#da2a41')
+            .setAuthor({
+          name: 'IMR Music Bot',
+          iconURL: 'https://cdn.discordapp.com/attachments/738717131155308585/1169611570234523761/IMG_7353.gif?ex=65c4c6a8&is=65b251a8&hm=f0ffcfdf21d5f9e4e60d66e0fa9344784ed5250f80d811a846444dca59824608&',
+          url: 'https://discord.gg/7Kbf69DmQm'
+        })
+            .setDescription(`__**About me**__:\n\n I was originally created for IMR clan.`)
+            .setTimestamp();
+      interaction.reply({ embeds: [embed] }).catch(e => {});
+
+    } catch (e) {
+    console.error(e); 
+  }
+  },
+};
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+   
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
