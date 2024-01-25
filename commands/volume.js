c21926f1b1bcb44c130f4cd105361f331d85e196
REALXDLMAO <157639208+REALXDLMAO@users.noreply.github.com>
2024-01-26 00:48:40 +0400

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,7 +4,7 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "volume",
-  description: "Allows you to adjust the music volume.",
+  description: "Control the music volume.",
   permissions: "0x0000000000000800",
   options: [{
     name: 'volume',
@@ -17,14 +17,14 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: 'No music playing.', ephemeral: true });
       }
 
       const vol = parseInt(interaction.options.getInteger('volume'));
 
       if (!vol) {
         return interaction.reply({
-          content: `Current volume: **${queue.volume}** 🔊\nTo change the volume, type a number between \`1\` and \`${maxVol}\`.`,
+          content: `Current volume: **${queue.volume}**To change the volume, type a number between \`1\` and \`${maxVol}\`.`,
           ephemeral: true
         });
       }
@@ -46,15 +46,15 @@ module.exports = {
         const embed = new EmbedBuilder()
           .setColor('#d291fe')
           .setAuthor({
-        name: 'Your Music! Your Rules!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157528025739563088/5657-volume-icon.png?ex=6518ef7b&is=65179dfb&hm=1797c2830537a28b5c6a57564517cc509146d02383a69fb4239d7b5d55aceeed&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        name: 'Your music, your rules.',
+        iconURL: 'https://cdn.discordapp.com/attachments/738717131155308585/1169611570234523761/IMG_7353.gif?ex=65c4c6a8&is=65b251a8&hm=f0ffcfdf21d5f9e4e60d66e0fa9344784ed5250f80d811a846444dca59824608&', 
+        url: 'https://discord.gg/7Kbf69DmQm'
     })
           .setDescription(`**Adjusting Volume : ** **${vol}/${maxVol}**`);
 
         return interaction.reply({ embeds: [embed] });
       } else {
-        return interaction.reply({ content: '❌ Something went wrong while changing the volume.', ephemeral: true });
+        return interaction.reply({ content: 'Something went wrong while changing the volume.', ephemeral: true });
       }
     } catch (e) {
       console.error(e);
